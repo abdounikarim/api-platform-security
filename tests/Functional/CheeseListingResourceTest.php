f4d1a8f9ea26a3fc0dcf1dccbba9e907aae192c3
abdounikarim <abdounikarim@gmail.com>
2019-10-09 18:17:43 +0200

    Backport the API Platform 2.5 Test Tools

---
+++ tests/Functional/CheeseListingResourceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class CheeseListingResourceTest extends ApiTestCase
+{
+    public function testCreateCheeseListing()
+    {
+        $this->assertEquals(42, 42);
+    }
+}
