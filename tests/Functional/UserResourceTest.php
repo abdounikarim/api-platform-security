a786109781b02b265b636c6a91d4719dbb064357
Karim <abdounikarim@gmail.com>
2019-10-15 18:20:22 +0200

    Testing, Updating Roles & Refreshing Data

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\User;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
 class UserResourceTest extends CustomApiTestCase
@@ -54,5 +55,14 @@ class UserResourceTest extends CustomApiTestCase
         $data = $client->getResponse()->toArray();
         $this->assertArrayNotHasKey('phoneNumber', $data);
 
+        // refresh the user & elevate
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $user->setRoles(['ROLE_ADMIN']);
+        $em->flush();
+        $this->logIn($client, 'cheeseplease@example.com', 'foo');
+        $client->request('GET', '/api/users/'.$user->getId());
+        $this->assertJsonContains([
+            'phoneNumber' => '555.123.4567'
+        ]);
     }
 }
